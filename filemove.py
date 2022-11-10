80556209ae680fdde5dfb4d0099a1e23b13c5c10
name <mauroagustin.lopez@gmail.com>
2022-11-10 19:58:32 -0300

    primera versión

---
+++ filemove.py
@@ -0,0 +1,17 @@
+import os #para manipular archivos y directorios
+import datetime #para obtener datos de la fecha
+datos_f = str(datetime.datetime.now())
+
+def file_move ():
+    carpeta_actual = os.getcwd() + "/"#obener el directio de trabajo actual
+    carpeta_historial = carpeta_actual + "historial/"
+
+    for nombre_archivo in os.listdir(carpeta_actual):
+        nombre, extension = os.path.splitext(carpeta_actual + nombre_archivo)
+        nombre = nombre_archivo.split(".")
+        nuevo_nombre = nombre[0] + datos_f + extension
+        if extension == ".xlsx":
+            os.rename(carpeta_actual + nombre_archivo, carpeta_historial + nuevo_nombre)
+
+
+file_move()
---
+++ requisitos.md
@@ -0,0 +1,14 @@
+#Requisitos del proyecto
+aplicación en python para contabilizar y resumir los gastos individuales o de una empresa en un mes, ingresando en el programa python datos del gasto y el precio, y que python genere un excel con datos como el balance, promedio por categoría y gráficos de los gastos más elevados el programa utiliza la librería XlsxWriter
+
+-Balance
+
+-promedio por categoría
+
+-graficos
+
+
+
+#Funcionalidades opciones a añadir
+Usar el módulo os para guardar en un directorio un historial de todos los archivos exel generados por mes ordenados cronológicamente 
+
